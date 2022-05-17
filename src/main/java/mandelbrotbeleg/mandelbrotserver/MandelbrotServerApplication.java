42027a198f0e2d3777fb96639ddda435825c35f2
AlioLeuchtmannUni <s82067Q@informatik.htw-dresden.de>
2022-05-17 10:25:29 +0200

    fix app package

--- src/main/java/mandelbrotbeleg/mandelbrotserver/MandelbrotServerApplication.java
+++ src/main/java/mandelbrotbeleg/mandelbrotserver/MandelbrotServerApplication.java
@@ -1,4 +1,4 @@
-package mandelbrotbeleg.mandelbrotserver;
+package main.java.mandelbrotbeleg.mandelbrotserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
