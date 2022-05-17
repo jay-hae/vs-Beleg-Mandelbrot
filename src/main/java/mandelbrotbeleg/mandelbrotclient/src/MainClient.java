fd239e6c5ad93d3eae660c49942cacec3770c4be
AlioLeuchtmannUni <s82067Q@informatik.htw-dresden.de>
2022-05-17 10:14:25 +0200

    Rest Schnittstelle

---
+++ src/main/java/mandelbrotbeleg/mandelbrotclient/src/MainClient.java
@@ -0,0 +1,109 @@
+package main.java.mandelbrotbeleg.mandelbrotclient.src;
+
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import java.util.*;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+
+public class MainClient extends JPanel implements MouseWheelListener{
+
+    static MainClient mainClient;
+
+    List<BufferedImage> bufferedImages = new ArrayList<BufferedImage>();
+
+    int serverAmount = 0;
+
+    int width = 800, height = 600;
+
+    int zoom = 0;
+
+    public static void main(String avg[]) throws IOException
+    {
+        JFrame frame = new JFrame();
+        mainClient = new MainClient(frame);
+
+        frame.setResizable(false);
+        frame.add(mainClient);
+        frame.setVisible(true);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.addMouseWheelListener(mainClient);
+
+        // Proof of concept - Changing Model:
+
+        System.out.println(new File("imageOne.jpg").getCanonicalPath());
+        mainClient.bufferedImages.add(ImageIO.read(new File("imageOne.jpg")));
+        mainClient.bufferedImages.add(ImageIO.read(new File("imageTwo.png")));
+
+        mainClient.displayImage();
+    }
+
+    public MainClient(JFrame frame) throws IOException
+    {
+        this.setPreferredSize(new Dimension(width, height));
+    }
+
+    public void displayImage()
+    {
+        mainClient.repaint();
+    }
+
+    /* Wird noch nicht getriggered*/
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        zoom -= e.getUnitsToScroll();
+
+        System.out.println(e.getWheelRotation());
+        // bufferedImages = getNewImages(); // Change model
+        displayImage(); // Present
+    }
+
+    public List<BufferedImage> getNewImages()
+    {
+        int segmentWidth = (width / serverAmount);
+
+        List<BufferedImage> bufferedImages = new ArrayList<BufferedImage>(serverAmount);
+
+        /* Alio:
+        * */
+        for(int i = 0; i < serverAmount; i++)
+        {
+            int leftPos = segmentWidth * i;
+            int rightPos = leftPos + segmentWidth;
+            int topPos = 0;
+            int bottomPos = height;
+
+            // bufferedImages[i] = getImageSegment(zoom, leftPos, rightPos, topPos, bottomPos); // async
+        }
+
+        for(int i = 0; i < serverAmount; i++)
+        {
+            //wait for response
+
+            // bufferedImages.add(...)
+        }
+
+        return bufferedImages;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        for(int i = 0; i < bufferedImages.size(); i++)
+        {
+            Graphics2D g2d = (Graphics2D) g.create();
+            g2d.drawImage(bufferedImages.get(i), (width / bufferedImages.size()) * i, 0, this);
+            g2d.dispose();
+        }
+    }
+
+}
---
+++ src/main/java/mandelbrotbeleg/mandelbrotserver/MandelbrotServerRestRessource.java
@@ -0,0 +1,109 @@
+package mandelbrotbeleg.mandelbrotserver;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Base64;
+
+@CrossOrigin(origins = "null")
+@RestController
+public class MandelbrotServerRestRessource {
+
+
+    // nur Test aufruf
+    @GetMapping("/calcGet")
+    String calcPixel2(){
+
+        System.out.println("Get request made");
+        try{
+
+            // Beispiel für Anfrage von Client
+            /* Send Http Request
+             Werte einsetzen*/
+            URL server = new URL("http://localhost:8080/calcolino/600/600/2/0/0");
+            URLConnection connection = server.openConnection();
+            HttpURLConnection http = (HttpURLConnection)connection;
+            http.setRequestMethod("GET");
+            http.setDoOutput(true);
+            http.connect();
+
+            // Recieve Http Reqeuest
+            BufferedReader in = new BufferedReader(new InputStreamReader(http.getInputStream()));
+            String result="";
+            String line;
+            while((line = in.readLine()) != null){
+                result+=line;
+            }
+            in.close();
+
+            // Convert String back to image ...
+            InputStream inputStream = new ByteArrayInputStream(Base64.getDecoder().decode(result));
+            BufferedImage image = ImageIO.read(inputStream);
+            if(image==null){
+                return "image was null";
+            }
+
+            // test save  auf client unrelevant
+            ImageIO.write(image, "png", new File("imageTwo.png"));
+
+            return result;
+
+        }catch (Exception e){
+
+            System.out.println(e.getMessage());
+        }
+        return "none";
+    }
+
+    /*
+    * width: segment Width
+    * height: segment Height
+    * scale: pixel abstand in Zahlenebene
+    * originX: X Koordinate Ursprung Segment / Pixel1 in Zahlenebene
+    * originY: Y koordinate Ursprung in Segment / Pixel1 in Zahlenebene
+    * */
+    @GetMapping("/calcolino/{width}/{height}/{scale}/{originX}/{originY}")
+    HttpEntity<String> calcGet(
+            @PathVariable int width,
+            @PathVariable int height,
+            @PathVariable Double scale,
+            @PathVariable Double originX,
+            @PathVariable Double originY) {
+
+        System.out.println("Server got Request with:\n" +
+                "width: "+width+"\n"+
+                "height: "+height+"\n"+
+                "scale: "+scale+"\n"+
+                "originX: "+originX+"\n"+
+                "originY: "+originY+"\n");
+       try{
+           // test image
+           BufferedImage testImage = ImageIO.read(new File("imageTwo.png"));
+
+           // convert BufferedImage to string
+           ByteArrayOutputStream baos = new ByteArrayOutputStream();
+           boolean res = ImageIO.write(testImage, "png", baos);
+
+           //baos.close();
+           System.out.println("size: "+baos.size());
+
+           if(!res){
+               return new HttpEntity<>("Fehler beim Bild lesen");
+           }
+
+           byte[] bytes = baos.toByteArray();
+           return new HttpEntity<>(Base64.getEncoder().encodeToString(bytes));
+
+       }catch(Exception e){
+            System.out.println("Exception");
+       }
+        return new HttpEntity<>("Server had Error");
+
+    }
+
+}
