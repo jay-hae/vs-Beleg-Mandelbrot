89d05e4c52b2fd1779555d12ba5b4ac4be1d2c41
PowerOfCreation <questerdesura@gmail.com>
2022-06-16 13:01:58 +0200

    clean maven in start server

--- start-server-linux.sh
+++ start-server-linux.sh
@@ -1,2 +1,3 @@
+./mvnw clean
 ./mvnw package -DskipTests -T12
 java -jar target/mandelbrotServer-server.jar
--- start-server-windows.ps1
+++ start-server-windows.ps1
@@ -1,2 +1,3 @@
+.\mvnw.cmd clean
 .\mvnw.cmd package -DskipTests -T12
 java -jar target/mandelbrotServer-server.jar
